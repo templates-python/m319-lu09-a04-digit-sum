968797b84e3bbfa55733e12ede415c59f0899550
Marcel Suter <marcel.suter@bzz.ch>
2025-11-11 09:11:41 +0100

    cleanup NOACTION

--- digit_sum.py
+++ digit_sum.py
@@ -1,10 +1,8 @@
-#TODO: Erstelle die Funktion digit_sum
-
-
 def main():
     number = input('Bitte eine Ganzzahl eingeben für Quersummenberechnung> ')
     print(f"Die Quersumme ist: {digit_sum(number)}")
 
+#TODO: Erstelle die Funktion digit_sum
 
 if __name__ == '__main__':
     main()
